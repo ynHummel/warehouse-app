a2372efd59df898cd039bcc85db789d3bf96fb99
ynhummel <yurihummel@outlook.com>
2021-12-18 12:09:39 -0300

    Bug resolvido onde CPF de galpão poderia ser registrado com tamanho maior do que o permitido

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -3,5 +3,5 @@ class Warehouse < ApplicationRecord
             :state, :city, :postal_code, 
             :description, :total_area, :useful_area, presence: true
   validates :code, uniqueness: true
-  validates :postal_code, format: { with: /\d{5}-\d{3}/ }
+  validates :postal_code, format: { with: /\d{5}-\d{3}/ }, length: { is: 9 }
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'postal_code is required' do 
+    warehouse = Warehouse.new( 
+      name: 'São Luís', code: 'GRU', description: 'teste',
+      address: 'Av teste', city: 'São Luís', state: 'MA',
+      postal_code: '', total_area: 10000, useful_area: 8000 
+    )
+    result = warehouse.valid?
+    expect(result).to eq false
+  end
+
+  context 'invalid CEP format' do
+    it 'cep 757' do
+      warehouse = Warehouse.new( 
+        name: 'Guarulhos', code: 'GRU', description: 'teste',
+        address: 'Av teste', city: 'São Paulo', state: 'SP',
+        postal_code: '757', total_area: 10000, useful_area: 8000 
+      )
+
+      result = warehouse.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'cep 757996-00' do
+      warehouse = Warehouse.new( 
+        name: 'Guarulhos', code: 'GRU', description: 'teste',
+        address: 'Av teste', city: 'São Paulo', state: 'SP',
+        postal_code: '757996-00', total_area: 10000, useful_area: 8000 
+      )
+
+      result = warehouse.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'cep 12345-6789' do
+      warehouse = Warehouse.new( 
+        name: 'Guarulhos', code: 'GRU', description: 'teste',
+        address: 'Av teste', city: 'São Paulo', state: 'SP',
+        postal_code: '12345-6789', total_area: 10000, useful_area: 8000 
+      )
+
+      result = warehouse.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'cep aaaaa-aaa' do
+      warehouse = Warehouse.new( 
+        name: 'Guarulhos', code: 'GRU', description: 'teste',
+        address: 'Av teste', city: 'São Paulo', state: 'SP',
+        postal_code: 'aaaaa-aaa', total_area: 10000, useful_area: 8000 
+      )
+
+      result = warehouse.valid?
+
+      expect(result).to eq false
+    end
+    
+  end
+
+  it 'duplicate code' do
+    
+    warehouse = Warehouse.create( 
+      name: 'Guarulhos', code: 'GRU', description: 'teste',
+      address: 'Av teste', city: 'São Paulo', state: 'SP',
+      postal_code: '00000-000', total_area: 10000, useful_area: 8000 
+    )
+
+    warehouse2 = Warehouse.new( 
+      name: 'São Luís', code: 'GRU', description: 'teste',
+      address: 'Av teste', city: 'São Luís', state: 'MA',
+      postal_code: '00002-000', total_area: 10000, useful_area: 8000 
+    )
+
+    result = warehouse2.valid?
+
+    expect(result).to eq false
+  end
 end
