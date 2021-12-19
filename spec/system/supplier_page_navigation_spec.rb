6dd1898965aa3cbb231423e5eed57f9d8f985c18
ynhummel <yurihummel@outlook.com>
2021-12-19 01:24:58 -0300

    Página com listagem dos fornecedores criada

--- spec/system/supplier_page_navigation_spec.rb
+++ spec/system/supplier_page_navigation_spec.rb
@@ -1,11 +1,45 @@
 require 'rails_helper'
 
-describe 'Visitor opens the homepage' do
+describe 'Visitor opens the supplier page' do
   it 'successfully' do
     visit root_path
     click_on 'Fornecedores' 
 
     expect(page).to have_css('h1', text: 'Fornecedores Cadastrados')
+  end
+
+  it 'and can see all the registered suppliers' do
+    Supplier.create!( 
+      trading_name: 'Fantasy Supplier', company_name: 'FS fornecimentos SA',
+      cnpj: '12345678901234', address: 'Av dos Produtos',
+      email: 'fantasyprodutos@fornecimentos.com', telephone: '00000000' 
+    )
+
+    Supplier.create!( 
+      trading_name: 'Real Supplier', company_name: 'RS fornecimentos SA',
+      cnpj: '12341234567890', address: 'Av dos Serviços',
+      email: 'realprodutos@fornecimentos.com', telephone: '00000001'
+    )
+    
+    visit root_path
+    click_on 'Fornecedores'
+    
+    expect(page).to have_content('Fantasy Supplier')
+    expect(page).to have_content('FS fornecimentos SA')
+    expect(page).to have_content('12345678901234')
+    expect(page).to have_content('fantasyprodutos@fornecimentos.com')
+    
+    expect(page).to have_content('Real Supplier')
+    expect(page).to have_content('RS fornecimentos SA')
+    expect(page).to have_content('12345678901234')
+    expect(page).to have_content('realprodutos@fornecimentos.com')
+  end
+
+  it 'and there are no suppliers' do
+    visit root_path
+    click_on 'Fornecedores'
+
+    expect(page).to have_content 'Nenhum fornecedor cadastrado'
   end
 
 end
