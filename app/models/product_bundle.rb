bcd57e7715b5d8a1195ce7e7d4d00d670d84a770
ynhummel <yurihummel@outlook.com>
2021-12-27 22:34:01 -0300

    Adiciona geração automática de SKU para Product Bundles

--- app/models/product_bundle.rb
+++ app/models/product_bundle.rb
@@ -1,4 +1,18 @@
 class ProductBundle < ApplicationRecord
   has_many :product_bundle_items
   has_many :product_types, through: :product_bundle_items
+
+  before_create :generate_sku
+
+  private
+  def generate_sku
+    loop do
+      new_sku = SecureRandom.hex(10).upcase
+      if !ProductType.where(sku: new_sku).exists?
+        self.sku = "K#{new_sku}"
+        break
+      end
+    end
+  end
+
 end
--- spec/system/register_product_bundle_spec.rb
+++ spec/system/register_product_bundle_spec.rb
@@ -27,13 +27,11 @@ describe 'User registers a bundle' do
     visit root_path
     click_on 'Criar novo kit de produtos'
     fill_in 'Nome', with: 'Kit Degustação Miolo'
-    fill_in 'Código SKU', with: 'KV1234567890'
     check 'Vinho Tinto Miolo'
     check 'Vinho Branco Miolo'
     click_on 'Salvar'
 
     expect(page).to have_content 'Kit Degustação Miolo'
-    expect(page).to have_content 'KV1234567890'
     expect(page).to have_content 'Vinho Tinto Miolo'
     expect(page).to have_content 'Vinho Branco Miolo'
     expect(page).not_to have_content 'Vinho Rose Miolo'
