d381850361a7be62ee4b9d7e1dcb8108bb5f1f8b
ynhummel <yurihummel@outlook.com>
2021-12-29 11:45:11 -0300

    Cria teste para garantir que validações de ProductType continuam a valer mesmo em sua atualização

--- spec/system/edit_product_type_spec.rb
+++ spec/system/edit_product_type_spec.rb
@@ -70,4 +70,47 @@ describe 'User tries to edit ProductType details' do
     
   end
 
+  it 'successfully' do
+    supplier = Supplier.create!( 
+      trading_name: 'Vinícola Miolo', company_name: 'Miolo Fábrica de bebidas LTDA',
+      cnpj: '51905325000154', address: 'Avenida Cabernet, 100',
+      email: 'contato@miolovinhos.com', telephone: '71 1234-5678' 
+    )
+    Supplier.create!(trading_name: 'Cerâmicas Geek', 
+      company_name: 'Geek fornecimentos SA', cnpj: '12345678901234',
+      email: 'geekceramicas@fornecimentos.com' 
+    )
+    ProductType.create!( 
+      name: 'Vinho Tinto Miolo', height: 30, width: 10, length: 10,
+      weight: 800, supplier: supplier
+    )
+
+    visit root_path
+    click_on 'Fornecedores'
+    click_on 'Vinícola Miolo'
+    click_on 'Vinho Tinto Miolo'
+    click_on 'Editar'
+
+    fill_in 'Nome', with: 'Vinho Branco Miolo'
+    fill_in 'Peso', with: '-801'
+    fill_in 'Altura', with: ''
+    fill_in 'Largura', with: '0'
+    fill_in 'Profundidade', with: '-12'
+    select 'Cerâmicas Geek', from: 'Fornecedor'
+    click_on 'Salvar'
+
+    expect(page).to have_content('Não foi possível atualizar o modelo de produto')
+    expect(page).to have_content('Peso deve ser maior que 0')
+    expect(page).to have_content('Altura não é um número')
+    expect(page).to have_content('Altura não pode ficar em branco')
+    expect(page).to have_content('Largura deve ser maior que 0')
+    expect(page).to have_content('Profundidade deve ser maior que 0')
+    expect(page).not_to have_content('Modelo de produto atualizado com sucesso')
+    expect(page).not_to have_content('-801 gramas')
+    expect(page).not_to have_content('Dimensões:  x 01 x -12')
+
+
+    
+  end
+
 end
