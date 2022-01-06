04a698ce1d81474cc23f932b98d581e1a9a71712
ynhummel <yurihummel@outlook.com>
2022-01-06 11:25:54 -0300

    Adiciona código gerado automaticamente para entrada de itens em galpões

--- app/models/warehouse_item.rb
+++ app/models/warehouse_item.rb
@@ -1,4 +1,17 @@
 class WarehouseItem < ApplicationRecord
   belongs_to :warehouse
   belongs_to :product_type
+
+  before_create :generate_code
+
+  private
+  def generate_code
+    loop do
+      new_code = SecureRandom.alphanumeric(17).upcase
+      if !WarehouseItem.where(id_code: new_code).exists?
+        self.id_code = "#{self.product_type.name[0,3].upcase}#{new_code}"
+        break
+      end
+    end
+  end
 end
--- spec/models/warehouse_item_spec.rb
+++ spec/models/warehouse_item_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe WarehouseItem, type: :model do
+  it 'should generate an id code' do 
+    cat = ProductCategory.create!(name: 'Monitores')
+    supp1 = Supplier.create!( 
+      trading_name: 'Fantasy Supplier', company_name: 'FS fornecimentos SA',
+      cnpj: '12345678901234', address: 'Av dos Produtos',
+      email: 'fantasyprodutos@fornecimentos.com', telephone: '00000000' 
+    )
+    p1 = ProductType.create!(
+      name: 'Monitor Gamer', weight: 2000, supplier: supp1, 
+      height: 14, width: 10, length: 12, product_category: cat
+    )
+    w1 = Warehouse.create!( 
+      name: 'Guarulhos', code: 'GRU', description: 'teste',
+      address: 'Av teste', city: 'São Paulo', state: 'SP',
+      postal_code: '00000-000', total_area: 10000, useful_area: 8000 
+    )
+    wi = WarehouseItem.create!(warehouse: w1, product_type: p1)
+
+    expect(wi.id_code).not_to eq nil
+    expect(wi.id_code.length).to eq 20
+    expect(wi.id_code[0,3]).to eq 'MON'
+  end
 end
